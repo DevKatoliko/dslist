99219051cfb6db9b6d6825b8db266edda97879dc
rubens <rubenshds@gmail.com>
2025-05-19 21:46:30 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,10 +19,12 @@ public class Game {
 	@Column(name = "game_year") // 'year' is a reserved word of the database
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -38,7 +40,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platform;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -79,11 +81,11 @@ public class Game {
 	}
 	
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 	
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
 	}
 	
 	public Double getScore() {
