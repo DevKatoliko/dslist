dbf4d3ca345622f534568431fd0d43b5a50d695d
rubens <rubenshds@gmail.com>
2025-05-20 19:27:21 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -6,7 +6,7 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 @Entity
-@Table(name = "tb_beloging")
+@Table(name = "tb_belonging")
 public class Belonging {
 	@EmbeddedId
 	private BelongingPk id = new BelongingPk();
